4627b43eab1320fbf30916427358474f76f3f34f
Arun Jaiswal <arunpratap.71@gmail.com>
2014-06-24 11:23:13 +0530

    SplitView_Swift
    A sample COCOA application using SWIFT Language

--- Demo SplitView/AppDelegate.swift
+++ Demo SplitView/AppDelegate.swift
@@ -8,9 +8,11 @@
 
 import Cocoa
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate,NSSplitViewDelegate {
                             
     @IBOutlet var window: NSWindow
+    @IBOutlet var headerSplitView : NSSplitView
+    @IBOutlet var splitView     :  NSSplitView
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
@@ -22,5 +24,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    func splitView(splitView: NSSplitView!,
+        constrainMaxCoordinate proposedMax: CGFloat,
+        ofSubviewAt dividerIndex: Int) -> CGFloat
+    {
+        var size:CGFloat
+        size = 0.0
+        if(splitView == self.headerSplitView)
+        {
+            println("Header split view")
+            size = 40.0;
+        }
+        else if(splitView == self.splitView)
+        {
+            println("Split View")
+            if(dividerIndex == 0)
+            {
+                size = 150
+            }
+            else if (dividerIndex == 1)
+            {
+                size = 480;
+            }
+        }
+        return size;
+    }
+    
+    
+    func splitView(splitView: NSSplitView!,
+        constrainMinCoordinate proposedMin: CGFloat,
+        ofSubviewAt dividerIndex: Int) -> CGFloat
+    {
+        var size:CGFloat
+        size = 0.0
+        
+        if(splitView == self.splitView)
+        {
+            if(dividerIndex == 1)
+            {
+                
+                size = 330//set the minimum width of split view divider at index 1
+            }
+        }
+        return size
+    }
 }
 
