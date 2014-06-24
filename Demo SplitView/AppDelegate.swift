b9365dbd84c998aa432432eca8340245d120c855
Arun Jaiswal <arunpratap.71@gmail.com>
2014-06-24 11:15:23 +0530

    Initial Commit

---
+++ Demo SplitView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Demo SplitView
+//
+//  Created by Arun Jaiswal on 24/06/14.
+//  Copyright (c) 2014 IPLTech. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Demo SplitView/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Demo SplitView
+//
+//  Created by Arun Jaiswal on 24/06/14.
+//  Copyright (c) 2014 IPLTech. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
